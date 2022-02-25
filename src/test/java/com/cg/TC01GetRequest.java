d5952d531a0a229415d490b9911764d568d3508e
daneyallari <daneyallari@gmail.com>
2022-02-25 16:59:31 +0530

    fix the test fail

--- src/test/java/com/cg/TC01GetRequest.java
+++ src/test/java/com/cg/TC01GetRequest.java
@@ -30,7 +30,7 @@ public class TC01GetRequest {
 		// Status Code Validation
 
 		int actualStatusCode = response.getStatusCode();
-		int expectedStatusCode = 201;
+		int expectedStatusCode = 200;
 
 		assertEquals(actualStatusCode, expectedStatusCode);
 
